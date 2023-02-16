1b9af1b04d05acadf800d74702d166f8dc292022
ryant4 <trekell.dev@gmail.com>
2023-02-16 16:34:48 -0500

    Add skeleton HTML and CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
+import './css-reset.css';
 import './style.css';
 
 console.log(123);
-
-const testElement = document.createElement('h1');
-testElement.innerHTML = "This should be red";
-const pageContent = document.querySelector('#content');
-pageContent.appendChild(testElement);
