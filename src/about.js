09f31b489ee78a7faee48ac2866ea3f9e4b38bf8
ryant4 <trekell.dev@gmail.com>
2023-02-17 21:15:47 -0500

    Change about us section text

--- src/about.js
+++ src/about.js
@@ -14,7 +14,7 @@ export default function aboutUs() {
     }
 
     const hours = "Monday: Closed <br> Tuesday: Closed <br> Wednesday: Closed <br> Thursday: Closed <br> Friday: Closed <br> Saturday: Closed <br> Sunday: 11am-4pm"
-    createAboutCard("Our History", "We're super old, so you should come here. We don't really care either way.");
+    createAboutCard("Our History", "We're super old, so you should come here. We don't really care either way. Our founders are rich now and they're using all of the revenue to fund their #nomadiclifestyle. Thanks for coming!");
     createAboutCard("Hours", hours);
 
 };
